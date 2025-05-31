101a22df2643f6d007ad1e5c04bab2e693c49c00
Rodan Ramdam <rodan.ramdam@gmail.com>
2025-05-31 23:40:44 +0545

    feat(backend): save hashed password

--- apps/backend/src/services/auth.service.ts
+++ apps/backend/src/services/auth.service.ts
@@ -2,6 +2,9 @@ import { RegisterUserInput } from "../dto/auth.dto";
 import { AppError } from "../lib/errors";
 import logger from "../lib/logger";
 import { User, UserDocument } from "../models";
+import bcrypt from "bcrypt";
+
+const SALT_ROUNDS = 10;
 
 const getUserByEmail = async (email: string): Promise<UserDocument | null> => {
   logger.info("getUserByEmail called with email:", email);
@@ -17,7 +20,12 @@ export const registerUser = async (
     throw new AppError(400, "Email already registered");
   }
 
-  const user = new User(data);
+  const hashedPassword = await bcrypt.hash(data.password, SALT_ROUNDS);
+
+  const user = new User({
+    ...data,
+    password: hashedPassword,
+  });
   await user.save();
 
   return { user };
