47dc20f62554a1177ff98fb033193ad20583b32f
Rodan Ramdam <rodan.ramdam@gmail.com>
2025-06-01 10:29:09 +0545

    feat(frontend): initial frontend setup

---
+++ apps/frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import Button from 'primevue/button'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about"><Button label="Submit" /> </RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
